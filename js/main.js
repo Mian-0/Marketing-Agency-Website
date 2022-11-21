579790f3e81783535865d84767830fa478adf64c
Mian-0 <102787006+Mian-0@users.noreply.github.com>
2022-11-21 14:26:14 +0000

    Header Completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 //  Header Scroll
 
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
 
 // nav hide
 
