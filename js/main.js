c593389019293273a2748958c36042b1e5622c65
Mian-0 <102787006+Mian-0@users.noreply.github.com>
2022-11-22 06:53:17 +0000

    Landing Page Completed

--- js/main.js
+++ js/main.js
@@ -11,6 +11,35 @@ window.onscroll = function(){
 
 
 // nav hide
-
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+            navCollapse.classList.remove("show");
+    })
+})
 
 // Owl Carousel
+$(document).ready(function() {
+    $('.client-slider-section').owlCarousel({
+        items: 4,
+        loop:true,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:6
+            }
+        }
+    });
+    
+});
